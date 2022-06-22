d5393f87a22ddcd9c9d838ac5e729ac6b53a0f39
구교진 <kgj1103@naver.com>
2022-06-22 12:03:43 +0900

    up

--- Solution.cpp
+++ Solution.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <algorithm>
 #include <cmath>
+#include <bitset>
 
 using namespace std;
 
@@ -464,7 +465,399 @@ vector<string> solution23(vector<string> strings, int n)
 	return answer;
 }
 
+//두 정수 사이의 합
+long long solution24(int a, int b) 
+{
+	long long answer = 0;
+	long long min = 0;
+	long long max = 0;
+	if (a == b)return a;//a 와 b 가 같은지 확인
+	else if (a > b) 
+	{
+		max = a; min = b;
+	}
+	else {
+		max = b; min = a;
+	}
+	for (long long i = min; i <= max; i++)
+	{
+		answer += i;
+	}
+	
+	return answer;
+}
+
+//나누어 떨어지는 숫자 배열
+vector<int> solution25(vector<int> arr, int divisor) 
+{
+	vector<int> answer;
+	sort(arr.begin(), arr.end());
+	for (int i = 0; i < arr.size(); i++)
+	{
+		if (arr[i] % divisor == 0)
+		{
+			answer.push_back(arr[i]);
+		}
+	}
+	if (answer.empty())
+	{
+		answer.push_back(-1);
+	}
+	return answer;
+}
+
+//같은 숫자는 싫어
+vector<int> solution26(vector<int> arr)
+{
+	vector<int> answer;
+	answer.push_back(arr[0]);
+	for (int i = 1; i < arr.size(); i++)
+	{
+		if (arr[i] != arr[i - 1])
+		{
+			answer.push_back(arr[i]);
+		}
+	}
+	// [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+	cout << "Hello Cpp" << endl;
+
+	return answer;
+}
+
+//다트게임
+int solution27(string dartResult) 
+{
+	int answer = 0;
+	int num = 0;
+	int score = 0;
+	int temp = 0;
+	vector<int> vec;
+	for (int i = 0; i < dartResult.size(); i++)
+	{
+		if (dartResult[i] >= '0' && dartResult[i] <= '9')
+		{
+			if (dartResult[i + 1] == '0')
+			{
+				score = 10;
+				i++;
+			}
+			else {
+				score = atoi(&dartResult[i]);
+			}
+		}
+		else {
+			switch (dartResult[i])
+			{
+			case 'S':
+				vec.push_back(score);
+				break;
+			case 'D':
+				vec.push_back(pow(score, 2.0f));
+				break;
+			case 'T':
+				vec.push_back(pow(score, 3.0f));
+				break;
+			case '*':
+				temp = vec.back();
+				vec.pop_back();
+				vec.back() = vec.back() * 2;
+				vec.push_back((temp * 2));
+				break;
+			case '#':
+				vec.back() = vec.back() * (-1);
+				break;
+			}
+		}
+	}
+	for (int i = 0; i < vec.size(); i++)
+	{
+		answer += vec[i];
+	}
+	return answer;
+}
+
+//가운데 글자 가져오기
+string solution28(string s) 
+{
+	int l;
+	string answer = "";
+	if (s.length() % 2 == 0)
+	{
+		l = s.length() / 2;
+		answer = { s[l - 1], s[l] };
+	}
+	else
+	{
+		l = s.length() / 2;
+		answer = s[l];
+	}
+	return answer;
+}
+
+//비밀지도
+vector<string> solution29(int n, vector<int> arr1, vector<int> arr2) 
+{
+	vector<string> answer;
+	string bnum;
+	string ret;
+	long b;
+	for (int i = 0; i < n; i++)
+	{
+		b = stol(bitset<32>(arr1[i] | arr2[i]).to_string());
+		bnum = to_string(b);
+		if (bnum.length() < n);
+		{
+			for (int i = bnum.size(); i < n; i++)
+			ret += ' ';
+		}
+		for (int i = 0; i < bnum.length(); i++)
+		{
+			bnum[i] == '1' ? ret += '#' : ret += ' ';
+		}
+		answer.push_back(ret);
+		ret.clear();
+	}
+
+	return answer;
+}
+
+//부족한 금액 계산하기
+long long solution30(int price, int money, int count)
+{
+	long answer = 0;
+	long sum = 0;
+	for (int i = 1; i <= count; i++)
+	{
+		sum += price * i;
+	}
+	answer = money - sum;
+	return answer <= 0 ? answer * (-1) : answer = 0;
+}
+
+//나머지가 1이 되는 수
+int solution31(int n) 
+{
+	int answer = 0;
+	int count = 1;
+	vector<int> ret = {};
+	while (ret.empty())
+	{
+		if (n % count == 1)
+		{
+			ret.push_back(count);
+		}
+		count++;
+	}
+	return ret.back();
+}
+
+//최소직사각형
+int solution32(vector<vector<int>> sizes)
+{
+	int answer = 0;
+	int x = 0;
+	int y = 0;
+	for (int i = 0; i < sizes.size(); i++)
+	{
+		sort(sizes[i].begin(), sizes[i].end());
+		x < sizes[i][0] ? x = sizes[i][0] : x;
+		y < sizes[i][1] ? y = sizes[i][1] : y;
+	}
+	return x * y;
+}
+
+//2016년
+string solution33(int a, int b) 
+{
+	string answer = "";
+	vector<string> Dname{ "SUN","MON","TUE","WED","THU","FRI","SAT"};
+	int day = 0;
+	for (int i = 1; i < a; i++)
+	{
+		switch (i)
+		{
+		case 1:case 3:case 5:case 7:case 8:case 10:case 12:
+			day += 31;
+			break;
+		case 2:
+			day += 29;
+			break;
+		case 4:case 6:case 9:case 11:
+			day += 30;
+			break;
+		}
+	}
+	day += (b - 1);
+	day = (5 + day) % 7;
+	return Dname[day];
+}
+
+//두 개 뽑아서 더하기
+vector<int> solution34(vector<int> numbers)
+{
+	vector<int> answer;
+	for (int i = 0; i < numbers.size(); i++)
+	{
+		for (int j = i + 1; j < numbers.size(); j++)
+		{
+			answer.push_back(numbers[i] + numbers[j]);
+		}
+	}
+	sort(answer.begin(), answer.end());
+	answer.erase(unique(answer.begin(), answer.end()), answer.end());
+	return answer;
+}
+
+//예산
+int solution35(vector<int> d, int budget) 
+{
+	int answer = 0;
+	int sum = 0;
+	int count = 0;
+	sort(d.begin(), d.end());
+	for (int i = 0; i < d.size(); i++)
+	{
+		sum += d[i];
+		if (sum <= budget)
+		{
+			count++;
+		}
+		else
+		{
+			break;
+		}
+	}
+	return count;
+}
+
+//3진법 뒤집기
+int solution36(int n)
+{
+	int answer = 0;
+	string str;
+	while (n!=0)
+	{
+		str += to_string(n % 3);
+		n /= 3;
+	}
+	int len = str.length()-1;
+	for (auto i : str)
+	{
+		answer += atoi(&i) * pow(3.0f, len);
+		len--;
+	}
+	return answer;
+}
+
+//약수의 개수와 덧셈
+int solution37(int left, int right)
+{
+	int answer = 0;
+	int count = 0;
+	for (int i = left; i <= right; i++)
+	{
+		for (int j = 1; j <= i; j++)
+		{
+			if (i % j == 0)
+			{
+				count++;
+			}
+		}
+		count % 2 == 0 ? answer += i : answer -= i;
+		count = 0;
+	}
+	return answer;
+}
+
+//실패율
+bool compare(vector<float>& a, vector<float>& b)
+{
+	if (a[0] == b[0])return a < b;
+	else return a > b;
+}
+vector<int> solution38(int N, vector<int> stages) 
+{
+	vector<int> answer;
+	vector<vector<float>> vec;
+	int chell;
+	int f = 0;
+	float fail = 0.0f;
+	for (int i = 1; i <= N; i++)
+	{
+		chell = stages.size();
+		int len = chell;
+		for (int j = 0; j < len; j++)
+		{
+			if (stages[j] <= i)
+			{
+				stages.erase(stages.begin() + j);
+				f++;
+				len--;
+				j--;
+			}
+		}
+		if (chell == 0)fail = 0;
+		else fail = (float)f / chell;
+		f = 0;
+		vec.push_back({ fail,(float)i });
+	}
+	sort(vec.begin(), vec.end(),compare);
+	for (auto i : vec)
+	{
+		answer.push_back(i[1]);
+	}
+	return answer;
+}
+
+//폰켓몬
+int solution39(vector<int> nums)
+{
+	int answer = 0;
+	int choose = 0;
+	choose = nums.size() / 2;
+	sort(nums.begin(), nums.end());
+	nums.erase(unique(nums.begin(), nums.end()),nums.end());
+	for (int i = 0; i < choose; i++)
+	{
+		if (nums[i]==nums.back())
+		{
+			answer++;
+			break;
+		}
+		answer++;
+	}
+	return answer;
+}
+
+//체육복
+int solution40(int n, vector<int> lost, vector<int> reserve) 
+{
+	int answer = 0;
+	int arr[35] = {0, };
+	for (int i : reserve)arr[i] = 1;
+	for (int i : lost)arr[i] += -1;
+	for (int i = 1; i <= n; i++)
+	{
+		if (arr[i] == -1)
+		{
+			if (arr[i - 1] == 1)
+			{
+				arr[i - 1] = arr[i] = 0;
+			}
+			else if (arr[i + 1] == 1)
+			{
+				arr[i + 1] = arr[i] = 0;
+			}
+		}
+	}
+	for (int i = 1; i <= n; i++)
+	{
+		if (arr[i] != -1)answer++;
+	}
+	return answer;
+}
+
 void main()
 {
-	solution({"sdasa","sdsadsaf","sdasads"},2);
+	solution(5, { 2,4 }, { 3 });
 }
